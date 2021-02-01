6d69c4be14404dcfc380bf885d024e67804cfaae
Gilbert <gilbertduenas@users.noreply.github.com>
2021-02-02 07:56:28 +0900

    Update climate.py

--- climate.py
+++ climate.py
@@ -1,8 +1,8 @@
 # import modules
 import numpy as np
 import pandas as pd
-
 import sqlalchemy
+
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
